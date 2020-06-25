c867134feb08fe69844de731ce6d0158767db714
manzars <manzarshaikh69@gmail.com>
2020-06-26 00:49:22 +0530

    v2 complete

--- frontend/gui/src/containers/Layout/Layout.js
+++ frontend/gui/src/containers/Layout/Layout.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import './Layout.css'
 import { Link } from 'react-router-dom'
+import {connect} from 'react-redux'
+import * as actionTypes from '../../store/actions/auth'
 
 const { Header, Content, Footer } = Layout;
 
 function layout(props) {
 
+    const logoutHandler = () => {
+        props.authLogout()
+    }
+
     let menuItems = null
     if(props.isAuthenticated){
         menuItems = (
-            <Menu.Item key="2">
+            <Menu.Item key="2" onClick = {logoutHandler}>
                 Logout
             </Menu.Item>
         )
@@ -47,4 +53,11 @@ function layout(props) {
         </Layout>
     )
 }
-export default layout
+
+const mapDispatchToProps = dispatch => {
+    return {
+        authLogout: () => dispatch(actionTypes.logout())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(layout)
